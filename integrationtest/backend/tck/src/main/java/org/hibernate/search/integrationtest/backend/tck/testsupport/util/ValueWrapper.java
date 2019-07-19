c5fbf6046152c2dc32f81d14c52b3815a736369f
Yoann Rodière <yoann@hibernate.org>
2019-07-19 10:53:02 +0200

    HSEARCH-3649 Add a method to query the accepted input types of a ToDocumentFieldValueConverter

--- integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/testsupport/util/ValueWrapper.java
+++ integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/testsupport/util/ValueWrapper.java
@@ -25,6 +25,10 @@ import org.hibernate.search.util.impl.integrationtest.common.NormalizationUtils;
 public final class ValueWrapper<T> implements Normalizable<ValueWrapper<T>> {
 	public static <T> ToDocumentFieldValueConverter<ValueWrapper<T>, T> toIndexFieldConverter() {
 		return new ToDocumentFieldValueConverter<ValueWrapper<T>, T>() {
+			@Override
+			public boolean isValidInputType(Class<?> inputTypeCandidate) {
+				return ValueWrapper.class.isAssignableFrom( inputTypeCandidate );
+			}
 
 			@Override
 			public T convert(ValueWrapper<T> value, ToDocumentFieldValueConvertContext context) {
