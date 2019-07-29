675974bf8c9c598beebd93f73a7593c7489e6b7d
Yoann Rodière <yoann@hibernate.org>
2019-07-29 17:41:07 +0200

    HSEARCH-3649 Create dedicated classes for the request/extract context when querying
    This will be useful to centralize information for
    projections/aggregations/etc.

---
+++ backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/search/query/impl/ElasticsearchSearchQueryRequestContext.java
@@ -0,0 +1,41 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.elasticsearch.search.query.impl;
+
+import java.util.Map;
+
+import org.hibernate.search.backend.elasticsearch.search.projection.impl.SearchProjectionExtractContext;
+import org.hibernate.search.engine.mapper.session.context.spi.SessionContextImplementor;
+import org.hibernate.search.engine.search.loading.context.spi.LoadingContext;
+
+import com.google.gson.JsonObject;
+
+class ElasticsearchSearchQueryRequestContext {
+
+	private final SessionContextImplementor sessionContext;
+	private final LoadingContext<?, ?> loadingContext;
+	private final Map<SearchProjectionExtractContext.DistanceSortKey, Integer> distanceSorts;
+
+	ElasticsearchSearchQueryRequestContext(
+			SessionContextImplementor sessionContext,
+			LoadingContext<?, ?> loadingContext,
+			Map<SearchProjectionExtractContext.DistanceSortKey, Integer> distanceSorts) {
+		this.sessionContext = sessionContext;
+		this.loadingContext = loadingContext;
+		this.distanceSorts = distanceSorts;
+	}
+
+	ElasticsearchSearchQueryExtractContext createExtractContext(JsonObject responseBody) {
+		return new ElasticsearchSearchQueryExtractContext(
+				sessionContext,
+				loadingContext.getProjectionHitMapper(),
+				distanceSorts,
+				responseBody
+		);
+	}
+
+}
