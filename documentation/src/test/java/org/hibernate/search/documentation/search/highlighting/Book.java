bc7fb2f642ad376d96966e2bc1e37283c298c975
Yoann Rodière <yoann@hibernate.org>
2022-09-19 13:33:29 +0200

    HSEARCH-4394 Move main code from Java EE to Jakarta EE

--- documentation/src/test/java/org/hibernate/search/documentation/search/highlighting/Book.java
+++ documentation/src/test/java/org/hibernate/search/documentation/search/highlighting/Book.java
@@ -6,9 +6,9 @@
  */
 package org.hibernate.search.documentation.search.highlighting;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 import org.hibernate.search.engine.backend.types.Highlightable;
 import org.hibernate.search.engine.backend.types.Projectable;
