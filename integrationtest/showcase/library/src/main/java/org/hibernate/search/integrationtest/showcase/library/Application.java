7e4c4df19684cb7caa911a6b802a0e7299bbba70
Yoann Rodière <yoann@hibernate.org>
2022-10-21 10:42:25 +0200

    WIP Add support for running the app
    Command line: mvn spring-boot:run -Dspring-boot.run.profiles=dev,lucene

--- integrationtest/showcase/library/src/main/java/org/hibernate/search/integrationtest/showcase/library/Application.java
+++ integrationtest/showcase/library/src/main/java/org/hibernate/search/integrationtest/showcase/library/Application.java
@@ -6,14 +6,20 @@
  */
 package org.hibernate.search.integrationtest.showcase.library;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@SpringBootApplication
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @ComponentScan
 @EnableJpaRepositories
 public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
